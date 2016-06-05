baf3c823ff4a9fe8f6494e1d5b6cdc915093838c
breus32 <muller.09@mail.ru>
2016-06-05 18:01:52 +0300

    Матриця

---
+++ zavdannya3(3).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	
+	int mas[100][100], i, j, n;
+    
+    printf("Enter matrix size: ");
+    scanf("%d", &n);
+	
+	for(i = 0; i < n; i++){
+		for ( j = 0; j < n; j++){
+			if(i == j){
+				mas[i][j] = n - i;
+			} else {
+				mas[i][j] = 0;
+			}
+		}
+	}
+
+	for(i = 0; i < n; i++){
+		for(j = 0; j < n; j++){
+			printf("%3d", mas[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
